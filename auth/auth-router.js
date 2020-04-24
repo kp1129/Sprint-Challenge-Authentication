77071dedc54611f9583c838c57ef7516402fb250
kp1129 <katya.p.1129@gmail.com>
2020-04-24 09:25:49 -0700

    add jest/supertest tests for /register and /login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -19,7 +19,7 @@ router.post("/register", (req, res) => {
     user.password = hash;
     // save new user in the db
     Users.add(user)
-      .then((response) => res.status(201).json(response))
+      .then((response) => res.status(201).json({ message: "Registration successful", newUser: response }))
       .catch((error) =>
         res.status(500).json({ message: "Could not add new user at this time" })
       );
