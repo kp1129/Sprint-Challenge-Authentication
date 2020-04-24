f6e5aa6bb6da433b6e8e6159e235acce480eefc9
kp1129 <katya.p.1129@gmail.com>
2020-04-24 08:49:34 -0700

    implement /login, issue tokens; tested with postman

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
+const secrets = require('./secrets');
+const jwt = require('jsonwebtoken');
 const Users = require("./authModel");
 
 router.post("/register", (req, res) => {
@@ -25,7 +27,38 @@ router.post("/register", (req, res) => {
 });
 
 router.post("/login", (req, res) => {
-  // implement login
+  let { username, password } = req.body;
+
+  // first, find them by username
+  Users.findBy({ username })
+  .then(([user]) => {
+    // check that the passwords match
+    if( user && bcrypt.compareSync(password, user.password)){
+      // log them in
+      // produce a token
+      const token = generateToken(user);
+      // send token to the client
+      res.status(200).json({ message: "You are logged in!", token })
+    } else {
+      res.status(401).json({ message: "Check your username and/or password" })
+    }
+  })
+  .catch(error => res.status(500).json({ message: "Login failed" }))
 });
+
+function generateToken(user) {
+  const payload = {
+    userId: user.id,
+    username: user.username
+  };
+
+  const secret = secrets.jwtSecret;
+
+  const options = {
+    expiresIn: "1d"
+  };
+
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
--- auth/authModel.js
+++ auth/authModel.js
@@ -2,6 +2,7 @@ const db = require('../database/dbConfig');
 
 module.exports = {
     add,
+    findBy,
     findById
 }
 
@@ -9,6 +10,10 @@ async function add(user) {
     const [id] = await db('users').insert(user, "id");
 
     return findById(id)
+}
+
+function findBy(filter) {
+    return db('users').where(filter);
 }
 
 function findById(id) {
