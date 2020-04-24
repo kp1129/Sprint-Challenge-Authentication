a1f09faed853ff9bcb5c678af4bd84ba37c974bb
kp1129 <katya.p.1129@gmail.com>
2020-04-24 08:36:59 -0700

    implement /register, hash passwords; tested with postman

--- api/server.js
+++ api/server.js
@@ -12,6 +12,10 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
+
+server.get("/", (req, res) => {
+    res.status(200).json({ message: "api is ready to go" })
+})
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
 
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,30 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const Users = require("./authModel");
 
-router.post('/register', (req, res) => {
-  // implement registration
+router.post("/register", (req, res) => {
+  let user = req.body;
+  // both username and password are required
+  if (!user.username || !user.password) {
+    res
+      .status(400)
+      .json({ message: "Please provide both username and password" });
+  } else {
+    // hash the password
+    const rounds = process.env.HASH_ROUNDS || 14;
+    const hash = bcrypt.hashSync(user.password, rounds);
+    // store the hashed password
+    user.password = hash;
+    // save new user in the db
+    Users.add(user)
+      .then((response) => res.status(201).json(response))
+      .catch((error) =>
+        res.status(500).json({ message: "Could not add new user at this time" })
+      );
+  }
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
 });
 
